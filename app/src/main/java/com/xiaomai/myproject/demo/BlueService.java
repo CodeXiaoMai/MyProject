a2504617db0791fc33732553a1e0c70afd8270b9
xiaomai <949804781@qq.com>
2016-11-20 21:16:57 +0800

    添加3个题目

---
+++ app/src/main/java/com/xiaomai/myproject/demo/BlueService.java
@@ -0,0 +1,392 @@
+package com.xiaomai.myproject.demo;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
+import android.bluetooth.BluetoothSocket;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+/**
+ * 这是一个蓝牙间通信的项目，需要用利用蓝牙而进行设备之间的数据传递。
+ * 请在代码区No.1处写出获取蓝牙连接的代码。在No.2处：写出实现发送数据功能的代码。
+ */
+public class BlueService {
+    private static final String TAG = "BlueService";
+    private static final boolean D = true;
+
+    private static final String NAME_SECURE = "BluetoothChatSecure";
+    private static final String NAME_INSECURE = "BluetoothChatInsecure";
+
+    private static final UUID MY_UUID_SECURE =
+            UUID.fromString("fa87c0d0-afac-11de-8a39-0800200c9a66");
+    private static final UUID MY_UUID_INSECURE =
+            UUID.fromString("8ce255c0-200a-11e0-ac64-0800200c9a66");
+
+    private final BluetoothAdapter mAdapter;
+    private final Handler mHandler;
+    private AcceptThread mSecureAcceptThread;
+    private AcceptThread mInsecureAcceptThread;
+    private ConnectThread mConnectThread;
+    private ConnectedThread mConnectedThread;
+    private int mState;
+
+    public static final int STATE_NONE = 0;       // we're doing nothing
+    public static final int STATE_LISTEN = 1;     // now listening for incoming connections
+    public static final int STATE_CONNECTING = 2; // now initiating an outgoing connection
+    public static final int STATE_CONNECTED = 3;  // now connected to a remote device
+
+    public BlueService(Context context, Handler handler) {
+        mAdapter = BluetoothAdapter.getDefaultAdapter();
+        mState = STATE_NONE;
+        mHandler = handler;
+    }
+
+    private synchronized void setState(int state) {
+        if (D) Log.d(TAG, "setState() " + mState + " -> " + state);
+        mState = state;
+
+        mHandler.obtainMessage(BluetoothChat.MESSAGE_STATE_CHANGE, state, -1).sendToTarget();
+    }
+
+    public synchronized int getState() {
+        return mState;
+    }
+
+    public synchronized void start() {
+        if (D) Log.d(TAG, "start");
+
+        if (mConnectThread != null) {
+            mConnectThread.cancel();
+            mConnectThread = null;
+        }
+
+        if (mConnectedThread != null) {
+            mConnectedThread.cancel();
+            mConnectedThread = null;
+        }
+
+        setState(STATE_LISTEN);
+
+        if (mSecureAcceptThread == null) {
+            mSecureAcceptThread = new AcceptThread(true);
+            mSecureAcceptThread.start();
+        }
+        if (mInsecureAcceptThread == null) {
+            mInsecureAcceptThread = new AcceptThread(false);
+            mInsecureAcceptThread.start();
+        }
+    }
+
+    public synchronized void connect(BluetoothDevice device, boolean secure) {
+        if (D) Log.d(TAG, "connect to: " + device);
+
+        if (mState == STATE_CONNECTING) {
+            if (mConnectThread != null) {
+                mConnectThread.cancel();
+                mConnectThread = null;
+            }
+        }
+
+        if (mConnectedThread != null) {
+            mConnectedThread.cancel();
+            mConnectedThread = null;
+        }
+
+        mConnectThread = new ConnectThread(device, secure);
+        mConnectThread.start();
+        setState(STATE_CONNECTING);
+    }
+
+    public synchronized void connected(BluetoothSocket socket, BluetoothDevice
+            device, final String socketType) {
+        if (D) Log.d(TAG, "connected, Socket Type:" + socketType);
+
+        if (mConnectThread != null) {
+            mConnectThread.cancel();
+            mConnectThread = null;
+        }
+
+        if (mConnectedThread != null) {
+            mConnectedThread.cancel();
+            mConnectedThread = null;
+        }
+
+        if (mSecureAcceptThread != null) {
+            mSecureAcceptThread.cancel();
+            mSecureAcceptThread = null;
+        }
+        if (mInsecureAcceptThread != null) {
+            mInsecureAcceptThread.cancel();
+            mInsecureAcceptThread = null;
+        }
+
+        mConnectedThread = new ConnectedThread(socket, socketType);
+        mConnectedThread.start();
+
+        Message msg = mHandler.obtainMessage(BluetoothChat.MESSAGE_DEVICE_NAME);
+        Bundle bundle = new Bundle();
+        bundle.putString(BluetoothChat.DEVICE_NAME, device.getName());
+        msg.setData(bundle);
+        mHandler.sendMessage(msg);
+
+        setState(STATE_CONNECTED);
+    }
+
+    public synchronized void stop() {
+        if (D) Log.d(TAG, "stop");
+
+        if (mConnectThread != null) {
+            mConnectThread.cancel();
+            mConnectThread = null;
+        }
+
+        if (mConnectedThread != null) {
+            mConnectedThread.cancel();
+            mConnectedThread = null;
+        }
+
+        if (mSecureAcceptThread != null) {
+            mSecureAcceptThread.cancel();
+            mSecureAcceptThread = null;
+        }
+
+        if (mInsecureAcceptThread != null) {
+            mInsecureAcceptThread.cancel();
+            mInsecureAcceptThread = null;
+        }
+        setState(STATE_NONE);
+    }
+
+    public void write(byte[] out) {
+        ConnectedThread r;
+        synchronized (this) {
+            if (mState != STATE_CONNECTED) return;
+            r = mConnectedThread;
+        }
+        r.write(out);
+    }
+
+    private void connectionFailed() {
+        Message msg = mHandler.obtainMessage(BluetoothChat.MESSAGE_TOAST);
+        Bundle bundle = new Bundle();
+        bundle.putString(BluetoothChat.TOAST, "Unable to connect device");
+        msg.setData(bundle);
+        mHandler.sendMessage(msg);
+
+        BlueService.this.start();
+    }
+
+    private void connectionLost() {
+        Message msg = mHandler.obtainMessage(BluetoothChat.MESSAGE_TOAST);
+        Bundle bundle = new Bundle();
+        bundle.putString(BluetoothChat.TOAST, "Device connection was lost");
+        msg.setData(bundle);
+        mHandler.sendMessage(msg);
+
+        BlueService.this.start();
+    }
+
+    private class AcceptThread extends Thread {
+        private final BluetoothServerSocket mmServerSocket;
+        private String mSocketType;
+
+        public AcceptThread(boolean secure) {
+            BluetoothServerSocket tmp = null;
+            mSocketType = secure ? "Secure" : "Insecure";
+
+            try {
+                if (secure) {
+                    tmp = mAdapter.listenUsingRfcommWithServiceRecord(NAME_SECURE,
+                            MY_UUID_SECURE);
+                } else {
+                    tmp = mAdapter.listenUsingInsecureRfcommWithServiceRecord(
+                            NAME_INSECURE, MY_UUID_INSECURE);
+                }
+            } catch (IOException e) {
+                Log.e(TAG, "Socket Type: " + mSocketType + "listen() failed", e);
+            }
+            mmServerSocket = tmp;
+        }
+
+        public void run() {
+            if (D) Log.d(TAG, "Socket Type: " + mSocketType +
+                    "BEGIN mAcceptThread" + this);
+            setName("AcceptThread" + mSocketType);
+
+            BluetoothSocket socket = null;
+
+            while (mState != STATE_CONNECTED) {
+                try {
+                    socket = mmServerSocket.accept();
+                } catch (IOException e) {
+                    Log.e(TAG, "Socket Type: " + mSocketType + "accept() failed", e);
+                    break;
+                }
+
+                if (socket != null) {
+                    synchronized (BlueService.this) {
+                        switch (mState) {
+                            case STATE_LISTEN:
+                            case STATE_CONNECTING:
+                                connected(socket, socket.getRemoteDevice(),
+                                        mSocketType);
+                                break;
+                            case STATE_NONE:
+                            case STATE_CONNECTED:
+                                try {
+                                    socket.close();
+                                } catch (IOException e) {
+                                    Log.e(TAG, "Could not close unwanted socket", e);
+                                }
+                                break;
+                        }
+                    }
+                }
+            }
+            if (D) Log.i(TAG, "END mAcceptThread, socket Type: " + mSocketType);
+
+        }
+
+        public void cancel() {
+            if (D) Log.d(TAG, "Socket Type" + mSocketType + "cancel " + this);
+            try {
+                mmServerSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "Socket Type" + mSocketType + "close() of server failed", e);
+            }
+        }
+    }
+
+
+    private class ConnectThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final BluetoothDevice mmDevice;
+        private String mSocketType;
+
+        public ConnectThread(BluetoothDevice device, boolean secure) {
+            mmDevice = device;
+            BluetoothSocket tmp = null;
+            mSocketType = secure ? "Secure" : "Insecure";
+
+            try {
+                //No.1
+                //开始写代码,根据代码写出得到的BluetoothSocket用于与连接的代码.
+
+
+                //end_code
+            } catch (IOException e) {
+                Log.e(TAG, "Socket Type: " + mSocketType + "create() failed", e);
+            }
+            mmSocket = tmp;
+        }
+
+        public void run() {
+            Log.i(TAG, "BEGIN mConnectThread SocketType:" + mSocketType);
+            setName("ConnectThread" + mSocketType);
+
+            mAdapter.cancelDiscovery();
+
+            try {
+                mmSocket.connect();
+            } catch (IOException e) {
+                try {
+                    mmSocket.close();
+                } catch (IOException e2) {
+                    Log.e(TAG, "unable to close() " + mSocketType +
+                            " socket during connection failure", e2);
+                }
+                connectionFailed();
+                return;
+            }
+
+            synchronized (BlueService.this) {
+                mConnectThread = null;
+            }
+
+            connected(mmSocket, mmDevice, mSocketType);
+        }
+
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "close() of connect " + mSocketType + " socket failed", e);
+            }
+        }
+    }
+
+    private class ConnectedThread extends Thread {
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+
+        public ConnectedThread(BluetoothSocket socket, String socketType) {
+            Log.d(TAG, "create ConnectedThread: " + socketType);
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) {
+                Log.e(TAG, "temp sockets not created", e);
+            }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        public void run() {
+            Log.i(TAG, "BEGIN mConnectedThread");
+            byte[] buffer = new byte[1024];
+            int bytes;
+
+            //No.2
+            //开始写代码，在连接的同时 将 InputStream 获取到放入bytes中，并利用Handler发送获取到输入的数据。如果出现异常请再次启动蓝牙这个服务。
+            while (true) {
+                try {
+                    bytes =
+
+
+                } catch (IOException e) {
+                    Log.e(TAG, "disconnected", e);
+                    connectionLost();
+
+
+                    break;
+                }
+
+            }
+            //end_code
+        }
+
+        public void write(byte[] buffer) {
+            try {
+                mmOutStream.write(buffer);
+
+                mHandler.obtainMessage(BluetoothChat.MESSAGE_WRITE, -1, -1, buffer)
+                        .sendToTarget();
+            } catch (IOException e) {
+                Log.e(TAG, "Exception during write", e);
+            }
+        }
+
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) {
+                Log.e(TAG, "close() of connect socket failed", e);
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/xiaomai/myproject/demo/TuYa.java
@@ -0,0 +1,126 @@
+package com.xiaomai.myproject.demo;
+
+/**
+ * Created by XiaoMai on 2016/11/20.
+ */
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class TuYa extends Activity {
+
+    private Button button_save, button_resume;
+    private ImageView iv_canvas;
+    private Bitmap baseBitmap;
+    private Canvas canvas;
+    /*声明画笔*/
+    private Paint mPaint;
+    /*手势监听器*/
+    private View.OnTouchListener onTouchListener = new OnTouchListener() {
+        /*表示手指在屏幕上X轴Y轴的坐标点*/
+        float startX;
+        float startY;
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    //No.1
+                    //开始写代码，请用代码实现初始化Bitmap对象、定义画布颜色（请定义为白色）、记录用户手指在屏幕上的触摸点坐标 将坐标存入到startX和startY变量中。
+
+
+                    //end_code
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                    //No.2
+                    //开始写代码，根据手指在屏幕滑动的轨迹实现绘制轨迹的线条，并更新手指在屏幕上的坐标点（startX和startY变量），将绘制好的图像展示在ImageView中。
+                    float stopX =
+                    float stopY =
+
+
+                    //end_code
+                    break;
+                case MotionEvent.ACTION_UP:
+
+                    break;
+
+                default:
+                    break;
+            }
+            return true;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        /*初始化画笔 设置画笔的宽度和颜色*/
+        mPaint = new Paint();
+        mPaint.setStrokeWidth(5);
+        mPaint.setColor(Color.RED);
+
+        iv_canvas = (ImageView) findViewById(R.id.iv_canvas);
+        button_save = (Button) findViewById(R.id.button_save);
+        button_resume = (Button) findViewById(R.id.button_resume);
+		/*保存按钮的监听器*/
+        button_save.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                saveBitmap();
+            }
+        });
+		/*清除按钮的监听器*/
+        button_resume.setOnClickListener(new OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                resumeCanvas();
+            }
+        });
+        iv_canvas.setOnTouchListener(onTouchListener);
+    }
+
+    /*保存图片的方法*/
+    protected void saveBitmap() {
+        try {
+			/*以png的格式保存的手机内存中*/
+            //No.3
+            //开始写代码，请在这里实现文件存储的方法，将手指在屏幕上涂鸦的图形以PNG图片的形式保存到SD卡中；并发送广播进行扫描SD卡。
+
+            File file
+
+
+            //end_code
+        } catch (Exception e) {
+            Toast.makeText(MainActivity.this, "保存图片失败", 0).show();
+            e.printStackTrace();
+        }
+    }
+
+    /*清除画布的方法*/
+    protected void resumeCanvas() {
+        if (baseBitmap != null) {
+            baseBitmap = Bitmap.createBitmap(iv_canvas.getWidth(),
+                    iv_canvas.getHeight(), Bitmap.Config.ARGB_8888);
+            canvas = new Canvas(baseBitmap);
+            canvas.drawColor(Color.WHITE);
+            iv_canvas.setImageBitmap(baseBitmap);
+            Toast.makeText(MainActivity.this, "清除画板成功，可以重新开始绘图", 0).show();
+        }
+    }
+}
