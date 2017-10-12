100afc12cec38599947033af2c2957ece59e902b
xiaomai <949804781@qq.com>
2017-10-12 18:22:49 +0800

    修改预览方向，成功拍照

--- app/src/main/java/com/xiaomai/myproject/capturing/ControlCameraActivity.java
+++ app/src/main/java/com/xiaomai/myproject/capturing/ControlCameraActivity.java
@@ -2,11 +2,22 @@ package com.xiaomai.myproject.capturing;
 
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.xiaomai.myproject.R;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by XiaoMai on 2017/10/9.
@@ -16,6 +27,7 @@ public class ControlCameraActivity extends AppCompatActivity {
     private static final String TAG = "ControlCameraActivity";
     private Preview mPreview;
     private Camera mCamera;
+    private Button mButton;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -23,15 +35,64 @@ public class ControlCameraActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_control_camera);
         mPreview = (Preview) findViewById(R.id.preview_view);
+        mButton = (Button) findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mCamera != null) {
+                    mCamera.takePicture(null, null, mPictureCallBack);
+                }
+            }
+        });
     }
+
+    private Camera.PictureCallback mPictureCallBack = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] data, Camera camera) {
+            File pictureFile = null;
+            try {
+                pictureFile = getOutPutMediaFile();
+            } catch (IOException e) {
+            }
+
+            if (pictureFile == null) {
+                Log.d(TAG, "onPictureTaken: Error creating media file, check storage permissions:");
+                return;
+            }
+
+            try {
+                FileOutputStream fos = new FileOutputStream(pictureFile);
+                fos.write(data);
+                fos.close();
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "onPictureTaken: File not found:" + e.getMessage());
+            } catch (IOException e) {
+                Log.d(TAG, "onPictureTaken: Error accessing file; " + e.getMessage());
+            }
+        }
+    };
 
     @Override
     protected void onResume() {
         super.onResume();
         boolean safeCameraOpen = safeCameraOpen();
         if (safeCameraOpen) {
+            // 旋转方向
+            mCamera.setDisplayOrientation(90);
+            Camera.Parameters parameters = mCamera.getParameters();
+            List<String> focusModes = parameters.getSupportedFocusModes();
+            if (focusModes.contains(Camera.Parameters.FOCUS_MODE_AUTO)) {
+                parameters.setFocusMode(Camera.Parameters.FOCUS_MODE_AUTO);
+            }
+            mCamera.setParameters(parameters);
             mPreview.setCamera(mCamera);
         }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        releaseCameraAndPreview();
     }
 
     private boolean safeCameraOpen() {
@@ -55,5 +116,17 @@ public class ControlCameraActivity extends AppCompatActivity {
             mCamera.release();
             mCamera = null;
         }
+    }
+
+    public File getOutPutMediaFile() throws IOException{
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageName = "JPEG_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+
+        return File.createTempFile(
+                imageName,
+                ".jpg",
+                storageDir
+        );
     }
 }
